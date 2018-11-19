e61eccd390018b9b3daaf377503b8cf6ff51bffa
Nicolas Luzurier <43642007+hifrcy@users.noreply.github.com>
2018-11-19 14:29:53 +0100

    modification fichier nature

--- Nature.java
+++ Nature.java
@@ -2,18 +2,18 @@ public class Nature {
 
     public static void main(String[] args) {
 
-        Penguin pingou = new Penguin("Pingou");
+    /*    Penguin pingou = new Penguin("Pingou");
         pingou.dive();
         pingou.swimDown(3);
         pingou.swimUp(1);
         pingou.swimDown(4);
         pingou.swimUp(5);
         pingou.swimUp(1);
-        pingou.getOut();
+        pingou.getOut(); */
 
         Eagle eyeCherry = new Eagle("Eye Cherry");
         // TODO : décommente le code suivant afin de le tester
-        /*
+
         eyeCherry.takeOff();
         eyeCherry.ascend(120);
         eyeCherry.ascend(30);
@@ -22,6 +22,6 @@ public class Nature {
         eyeCherry.land();
         eyeCherry.descend(9);
         eyeCherry.land();
-        */
+
     }
 }
